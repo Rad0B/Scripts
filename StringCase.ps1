2e8e12a39b2847175ca01bd01f6d2e64ce2294b3
Radoslaw Borowiec <rbo@jndata.dk>
2023-06-04 07:35:11 +0200

    commit_0406

---
+++ StringCase.ps1
@@ -0,0 +1,32 @@
+function StringCase {
+
+    param(
+    [string[]]$String,
+    [switch]$ToLower,
+    [switch]$ToUpper,
+    [switch]$ToTitle
+    )
+
+    Begin{}
+    Process{
+
+        if($ToLower){
+        
+            Return $String.ToLower()
+
+        }
+        if($ToUpper){
+
+            Return $String.ToUpper()    
+
+        }
+        if($ToTitle){
+            foreach($str in $String){
+                (Get-Culture).TextInfo.ToTitleCase($str)
+            }
+        }
+        else{
+            Return $String
+        }
+    }
+}
